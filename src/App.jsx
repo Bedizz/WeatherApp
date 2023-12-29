d3f31125ca1161dda320aa5e05bd96e8cb11b0d9
unknown <turkyilmaz.bediz@gmail.com>
2023-12-29 23:33:15 +0100

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
-import { useState, useEffect } from 'react'
+import React ,{ useState, useEffect } from 'react'
 import Axios from 'axios'
 import './App.css'
+import './index.css'
 
 function App() {
   const [weather, setWeather] = useState({})
